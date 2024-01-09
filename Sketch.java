077545e1aeea37ff874932e0eb4bf98839891934
ChrisTarla <christine.tarla26@ycdsbk12.ca>
2024-01-09 09:22:07 -0500

    set up images

--- Sketch.java
+++ Sketch.java
@@ -1,13 +1,30 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
-		
+  PImage fallingPuff;
+  PImage divingPuff;
+  PImage glidingPuff;
+  PImage happyPuff;
+  PImage holdingPuff;
+  PImage multiplier;
+  PImage heart;
+  PImage background;
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
     size(400, 700);
+    // Assign images 
+    fallingPuff = loadImage("Comp Sci CPT - Falling Purple Puff.png");
+    divingPuff = loadImage("Comp Sci CPT - Diving Purple Puff Sprite.png");
+    glidingPuff = loadImage("Comp Sci CPT - Gliding Purple Puff Sprite.png");
+    happyPuff = loadImage("Comp Sci CPT - Happy Purple Puff Sprite.png");
+    holdingPuff = loadImage("Comp Sci CPT - Purple Puff Sprite Holding.png");
+    multiplier = loadImage("Green X.png");
+    heart = loadImage("Pixel Heart.png");
   }
 
   /** 
@@ -20,7 +37,9 @@ public class Sketch extends PApplet {
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
-  public void draw() {
-	  
+  public void draw(){
+	  //image(background, 0, 0);
+    happyPuff.resize(20, 20);
+    image(happyPuff, 200, 350);
   }
 }
