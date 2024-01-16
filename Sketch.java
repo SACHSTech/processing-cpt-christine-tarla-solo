0408d7ec42bf29e78772381e6bfecc6d8e61cb57
ChrisTarla <christine.tarla26@ycdsbk12.ca>
2024-01-16 10:53:56 -0500

    Added speed and sprite changes, edited obstacle Y and starting obstacles, and started boosters

--- Sketch.java
+++ Sketch.java
@@ -1,7 +1,6 @@
 /** Edit Ideas 
  * Add a starting and ending menu (space to start game) 
- * Change sprites with falling speed 
- * Add life and scroe boosters 
+ * Add life and score boosters 
  * Add score in top left corner 
  */
 
@@ -10,6 +9,7 @@ import processing.core.PImage;
 
 public class Sketch extends PApplet {
   // Declaring Image Variables 
+  PImage playerPuff;
   PImage fallingPuff;
   PImage divingPuff;
   PImage glidingPuff;
@@ -19,10 +19,10 @@ public class Sketch extends PApplet {
   PImage heart;
   PImage background;
   // Obstacle Location Variables 
-  // *DELETE AFTER* Maybe have more obstacles closer together if its too easy 
-  float[] fltObstacleY = {70, 140, 210, 280, 350, 420, 490, 560, 630, 700};
+  float[] fltObstacleY = new float[10];
   float[] fltObstacleX = new float[10];
-  int intObstacleSpeed = 2;
+  int intObsCounter = 700;
+  int intMovingSpeed = 2;
   // Player Movement Variables 
   int intPlayerX = 163;
   int intPlayerY = 313;
@@ -33,6 +33,12 @@ public class Sketch extends PApplet {
   // Player Lives Variables 
   int intLives = 3;
   boolean blnPlayerAlive = true;
+  // Booster Variables 
+  int intLifeBoosterX;
+  int intLifeBoosterY;
+  int intScoreBoosterX;
+  int intScoreBoosterY;
+  int intBoosterTiming = 1500;
 
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -42,6 +48,7 @@ public class Sketch extends PApplet {
     size(400, 700);
     // Assign images 
     fallingPuff = loadImage("/CPT Images/Falling Purple Puff.png");
+    fallingPuff.resize(75, 75);
     divingPuff = loadImage("/CPT Images/Diving Purple Puff Sprite.png");
     glidingPuff = loadImage("/CPT Images/Gliding Purple Puff Sprite.png");
     happyPuff = loadImage("/CPT Images/Happy Purple Puff Sprite.png");
@@ -49,6 +56,7 @@ public class Sketch extends PApplet {
     multiplier = loadImage("/CPT Images/Green X.png");
     heart = loadImage("/CPT Images/Pixel Heart.png");
     background = loadImage("/CPT Images/Yellow Cloud Background.png");
+    playerPuff = fallingPuff;
   }
 
   /** 
@@ -61,50 +69,58 @@ public class Sketch extends PApplet {
       // *DELETE AFTER* Potentially make every other X on the same side (use modulus; % = 0 is even, % = 1 is odd) to have a more even play 
       fltObstacleX[i] = random(0, 325);
     }
+
+    for (int j = 0; j < fltObstacleY.length; j++){
+      fltObstacleY[j] = j * 70 + 700;
+    }
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw(){
-    if (blnPlayerAlive == true){
-      background.resize(400, 700);
-      image(background, 0, 0);
+    background.resize(400, 700);
+    image(background, 0, 0);
 
-      // Draw Player Lives 
-      stroke(205, 25, 130);
-      fill(205, 25, 130);
-      if (intLives == 3){
-        rect(355, 5, 10, 10);
-        rect(370, 5, 10, 10);
-        rect(385, 5, 10, 10);
-      } else if (intLives == 2){
-        rect(570, 5, 10, 10);
-        rect(585, 5, 10, 10);
-      } else if (intLives == 1){
-        rect(585, 5, 10, 10);
+    image(playerPuff, intPlayerX, intPlayerY);
+    playerMovement();
+
+    stroke(160, 0, 165);
+    fill(160, 0, 165);
+    for (int i = 0; i < fltObstacleY.length; i++){
+      rect(fltObstacleX[i], fltObstacleY[i], 75, 15);
+      //playerCollision();
+      fltObstacleY[i] -= intMovingSpeed;
+
+      if (fltObstacleY[i] < 0) {
+        fltObstacleY[i] = 700;
       }
+    }
 
-      fallingPuff.resize(75, 75);
-      image(fallingPuff, intPlayerX, intPlayerY);
-
-      stroke(160, 0, 165);
-      fill(160, 0, 165);
-      for (int i = 0; i < fltObstacleY.length; i++){
-        rect(fltObstacleX[i], fltObstacleY[i], 75, 15);
-        playerCollision();
-        fltObstacleY[i] -= intObstacleSpeed;
-
-        if (fltObstacleY[i] < 0) {
-          fltObstacleY[i] = 700;
-        }
+    /*intObsCounter -= intMovingSpeed;
+    if (intObsCounter == 0){
+      for (int j = 0; j < fltObstacleY.length; j++){
+        fltObstacleY[j] = j * 70;
       }
+      intObsCounter = 700;
+    }*/
 
-      playerMovement();
-    } else {
-      // End Game 
-      // EDIT 
-      background(255);
+    // Draw Player Lives 
+    stroke(205, 25, 130);
+    fill(205, 25, 130);
+    if (intLives == 3){
+      rect(355, 5, 10, 10);
+      rect(370, 5, 10, 10);
+      rect(385, 5, 10, 10);
+    } else if (intLives == 2){
+      rect(370, 5, 10, 10);
+      rect(385, 5, 10, 10);
+    } else if (intLives == 1){
+      rect(385, 5, 10, 10);
+    }
+
+    if (blnPlayerAlive == false){
+      noLoop();
     }
   }
 
@@ -112,11 +128,17 @@ public class Sketch extends PApplet {
    * A method that changes the x and y value of the provided variables based on the conditions of the user keyboard input 
    */
   public void playerMovement(){
-    if (blnUp == true && intPlayerY >= 25){
-      intPlayerY -= 3;
-    } else if (blnDown == true && intPlayerY <= 725){
-      intPlayerY += 3;
-    } else if (blnLeft == true && intPlayerX >= 25){
+    if (blnUp == true){
+      intMovingSpeed = 1;
+      playerPuff = glidingPuff;
+      playerPuff.resize(65, 65);
+    } else if (blnDown == true){
+      intMovingSpeed = 4;
+      playerPuff = divingPuff;
+      playerPuff.resize(75, 60);
+    } 
+    
+    if (blnLeft == true && intPlayerX >= -25){
       intPlayerX -= 3;
     } else if (blnRight == true && intPlayerX <= 375){
       intPlayerX += 3;
@@ -135,13 +157,7 @@ public class Sketch extends PApplet {
       blnDown = true;
     } else if (key == 'D' || key == 'd'){
       blnRight = true;
-    } else if (key == CODED){
-      if (keyCode == UP){
-        intObstacleSpeed = 1;
-      } else if (keyCode == DOWN){
-        intObstacleSpeed = 4;
-      }
-    }
+    } 
   }
 
   /**
@@ -150,29 +166,27 @@ public class Sketch extends PApplet {
   public void keyReleased(){
     if (key == 'W' || key == 'w'){
       blnUp = false;
+      intMovingSpeed = 2;
+      playerPuff = fallingPuff;
     } else if (key == 'A' || key == 'a'){
       blnLeft = false;
     } else if (key == 'S' || key == 's'){
       blnDown = false;
+      intMovingSpeed = 2;
+      playerPuff = fallingPuff;
     } else if (key == 'D' || key == 'd'){
       blnRight = false;
-    } else if (key == CODED){
-      if (keyCode == UP){
-        intObstacleSpeed = 2;
-      } else if (keyCode == DOWN){
-        intObstacleSpeed = 2;
-      }
-    }
+    } 
   }
 
-/**
+  /**
    * A method that detects if the player circle collides with the snow, and deducts a life accordingly 
    */
   public void playerCollision(){
     for (int i = 0; i < fltObstacleY.length; i++){
-      if (intPlayerY - 38 < fltObstacleY[i] + 15 && intPlayerY + 38 > fltObstacleY[i]){
-        if (intPlayerX - 38 < fltObstacleX[i] + 25 && intPlayerX + 38 > fltObstacleX[i] - 25){        
-          fltObstacleY[i] -= 80;
+      if (intPlayerY + 5 < fltObstacleY[i] + 15 && intPlayerY + 70 > fltObstacleY[i]){
+        if (intPlayerX + 5 < fltObstacleX[i] + 75 && intPlayerX + 70 > fltObstacleX[i]){        
+          fltObstacleY[i] = 700;
           intLives --;
           updateLives();
         }
